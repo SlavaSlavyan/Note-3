dc5039842bb626b4a7657f67c093ec1afb0528a4
Svyatoslav <tortik2000.g@gmail.com>
2026-09-11 10:05:47 +0300

    Update

---
+++ Tests/260910/task 4/code.py
@@ -0,0 +1,9 @@
+num1: int = int(input("Number 1 << "))
+num2: int = int(input("Number 2 << "))
+num3: int = int(input("Number 3 << "))
+
+result: float = (num1 + num2 + num3) / 3.0
+
+print(f"\nArithmetic mean {int(result)} [{result}]\n\nPress ENTER to exit...\n")
+
+input()
